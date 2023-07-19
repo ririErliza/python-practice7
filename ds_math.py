51f792bf7d3a20882c00665593952a88184e8a25
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-19 11:05:02 +0200

    Update ds_math.py

---
+++ ds_math.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+'''------------------------------------------------------------'''
+'''-----------------    Linear Functions   --------------------'''
+'''------------------------------------------------------------'''
+
+# A linear function has one independent variable (x) and one
+#  dependent variable (y), and has the following form:
+
+# y = f(x) = ax + b
+# This function is used to calculate a value for the dependent
+#  variable when we choose a value for the independent variable.
+
+# Explanation:
+
+# f(x) = the output (the dependant variable)
+# x = the input (the independant variable)
+# a = slope = is the coefficient of the independent variable.
+#  It gives the rate of change of the dependent variable
+# b = intercept = is the value of the dependent variable when x = 0.
+#  It is also the point where the diagonal line crosses the vertical
+#  axis.
+
+
