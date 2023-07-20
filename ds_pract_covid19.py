e55f73c7de0a1887d89c10bc358613cff15341d3
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-20 09:23:26 +0200

    Create ds_pract_covid19.py

---
+++ ds_pract_covid19.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as ani
+import numpy as np
+
+GREY = (0.78, 0.78, 0.78) #uninfected
+RED = (0.96, 0.15, 0.15) #infected
+GREEN = (0, 0.86, 0.03) #recovered
+BLACK = (0,0,0) #dead
+
+COVID19_PARAMS = {
+    "r0": 2.28,
+    "incubation":5,
+    "percent_mild": 0.8,
+    "mild_recovery": (7,14),
+    "percent_severe": 0.2,
+    "severe_recovery":(21,42),
+    "severe_death":(14,56),
+    "fatality_rate":0.034,
+    "serial_interval":7
+}
