e73b464af62ef7468d46777d4edfdd139f5ca7ef
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-21 11:12:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -60,13 +60,32 @@ s = pd.Series([np.nan, 1, 2, np.nan, 3])
 s = s.fillna(method='ffill')
 
 print(s)
+##########################################
 
 a = np.array(([1,2,3,4,5], [6,7,8,9,10]))
 b = np.max(a, axis=1)
 
 print("b: ", b.sum())
 
+######################################
+
 a = np.ones((2,4))
 print(a)
 b = a.reshape((4,2))
 print(b)
+
+#####################################
+
+filedata = np.genfromtxt('data.txt', delimiter=',')
+output = np.any(filedata<50)
+print("output any: ", output)
+# output any:  True
+
+output = np.all(filedata<50, axis = 1)
+print("output all: ", output)
+# output all:  [False False]
+
+output = filedata[filedata<50]
+print(output)
+# [29. 32. 45. 15.  5. 22.]
+
